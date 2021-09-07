779568924cfc0a9dbdf0df9c6aa72c0d88a1c2e9
ErickBareiro <erick-bareiro@hotmail.com>
2021-09-07 12:32:08 -0400

    bug do menu resolvido

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 const btnMobile = document.getElementById('btn-mobile');
 
+const close1 = document.getElementById('close-1');
+const close2 = document.getElementById('close-2');
+const close3 = document.getElementById('close-3');
+const close4 = document.getElementById('close-4');
+
+
 function toggleMenu(event) {
   if (event.type === 'touchstart') event.preventDefault();
   const nav = document.getElementById('nav');
@@ -12,6 +18,17 @@ function toggleMenu(event) {
     event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
   }
 }
+
+function closeMenu() {
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+
+}
+
+close1.addEventListener('click', closeMenu);
+close2.addEventListener('click', closeMenu);
+close3.addEventListener('click', closeMenu);
+close4.addEventListener('click', closeMenu);
 
 btnMobile.addEventListener('click', toggleMenu);
 btnMobile.addEventListener('touchstart', toggleMenu);
